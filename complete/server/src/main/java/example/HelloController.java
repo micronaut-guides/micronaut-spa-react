c9feac236086b3b92ba0004abb0b2c22b7a86a63
Sergio del Amo <sergio.delamo@softamo.com>
2019-03-07 10:44:33 +0100

    Controllers returning a string should produce text-plain

--- complete/server/src/main/java/example/HelloController.java
+++ complete/server/src/main/java/example/HelloController.java
@@ -1,12 +1,15 @@
 package example;
 
 import io.micronaut.http.HttpResponse;
+import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Produces;
 
 @Controller("/")
 public class HelloController {
 
+    @Produces(MediaType.TEXT_PLAIN)
     @Get("/{name}")
     HttpResponse<String> hello(String name) {
         return HttpResponse.ok("Hello, " + name + "!");
